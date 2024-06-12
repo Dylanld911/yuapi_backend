8160b0e28d633c876d6f5c326dc5df41a051b8c7
Dylanld911 <1049100163@qq.com>
2024-06-12 13:47:10 +0800

    流量染色 全局过滤器

---
+++ yuapi-interface/src/main/java/com/yupi/yuapiinterface/config/FilterConfig.java
@@ -0,0 +1,22 @@
+package com.yupi.yuapiinterface.config;
+
+/**
+ * yuapi-interface com.yupi.yuapiinterface.config
+ * 2024/6/12 12:57
+ * @Author LD
+ */
+
+// @Configuration
+// public class FilterConfig {
+//
+//     @Bean
+//     public FilterRegistrationBean<RequestHeaderFilter> loggingFilter(){
+//         FilterRegistrationBean<RequestHeaderFilter> registrationBean = new FilterRegistrationBean<>();
+//
+//         registrationBean.setFilter(new RequestHeaderFilter());
+//         registrationBean.addUrlPatterns("/api/**"); // Adjust the URL pattern as needed
+//
+//         return registrationBean;
+//     }
+// }
+
